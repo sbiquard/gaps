b0464db736b0140ebd8946f06d4c771233a3b9f7
Simon Biquard <sbiquard+git@gmail.com>
2025-07-21 12:14:50 +0100

    add sim_noise, baseline and sim_constrained_block to utils

--- utils_mappraiser.py
+++ utils_mappraiser.py
@@ -2,6 +2,10 @@ import numpy as np
 import numpy.typing as npt
 from scipy.optimize import curve_fit
 from scipy.signal import get_window
+from toast import rng
+from toast.ops.sim_tod_noise import sim_noise_timestream
+
+import mappraiser.wrapper as mappraiser
 
 WELCH_SEGMENT_DURATION = 300  # 5 minutes
 
@@ -154,3 +158,233 @@ def autocorr_to_psd(autocorr, fft_size: int):
     psd = np.abs(np.fft.rfft(kernel, n=fft_size))
     # psd[0] = 0
     return psd
+
+
+# ____________________________________________________________
+# TOD manipulation
+
+
+def sim_noise(
+    samples=0,
+    realization=0,
+    detindx=0,
+    sindx=0,
+    telescope=0,
+    fsamp=1.0,
+    py=False,
+    autocorr=None,
+    freq=None,
+    psd=None,
+    use_toast=False,
+    verbose=False,
+):
+    """
+    Simulates a noise timestream according to a given autocorrelation function / PSD.
+
+    Args:
+        samples (int, optional): Number of samples to generate
+        realization (int, optional): RNG realization
+        detindx (int, optional): Detector index
+        sindx (int, optional): Session index
+        telescope (int, optional): Telescope ID
+        fsamp (float, optional): Sample rate
+        py (bool, optional): Whether to use the Python implementation instead of C. Defaults to False.
+        autocorr (NDArray, optional): The autocorrelation function. Is needed to use the C routine.
+        freq (NDArray, optional): Array containing the PSD frequencies. Is not needed if `autocorr` is given.
+        psd (NDArray, optional): Array containing the PSD values. Is not needed if `autocorr` is given. For use with the Python implementation, must contain exactly nfft // 2 + 1 values, where nfft is the next fast FFT size above `samples`.
+        use_toast (bool, optional): Whether to use TOAST's timestream generation. If True, the user must provide `freq` and `psd`. Defaults to False.
+
+    Returns:
+        NDArray: The generated timestream
+    """
+    # Do not use TOAST if autocorr is given
+    if autocorr is not None:
+        use_toast = False
+
+    if use_toast:
+        if verbose:
+            print('sim_noise: use TOAST routine')
+        if freq is None:
+            raise ValueError('freq must be provided when using TOAST routine')
+        if psd is None:
+            raise ValueError('psd must be provided when using TOAST routine')
+        return sim_noise_timestream(
+            realization=realization,
+            telescope=telescope,
+            sindx=sindx,
+            detindx=detindx,
+            rate=fsamp,
+            samples=samples,
+            freq=freq,
+            psd=psd,
+        )
+
+    # Using C implementation (mappraiser)
+    if not py:
+        if autocorr is None:
+            raise ValueError('autocorr must be provided if using C routine')
+        if verbose:
+            print('sim_noise: use C routine')
+        tdata = np.zeros(samples)
+        mappraiser.sim_noise_tod(
+            samples,
+            len(autocorr),
+            autocorr,
+            tdata,
+            realization,
+            detindx,
+            sindx,
+            telescope,
+            fsamp,
+        )
+        return tdata
+
+    # Using Python implementation (similar to TOAST but no resampling of PSD is done)
+    if verbose:
+        print('sim_noise: use Python code')
+    fftlen = 2
+    while fftlen <= samples:
+        fftlen *= 2
+    npsd = fftlen // 2 + 1
+
+    norm = fsamp * float(npsd - 1)
+
+    if autocorr is not None:
+        # Compute the PSD
+        lag = len(autocorr)
+        circ_t = np.pad(autocorr, (0, fftlen - lag), 'constant')
+        if lag > 1:
+            circ_t[-lag + 1 :] = np.flip(autocorr[1:], 0)
+        psd_final = np.abs(np.fft.rfft(circ_t, n=fftlen))
+    else:
+        # User wants to work directly with the PSD
+        # We don't do any resampling, input PSD must be of the right size
+        if psd is None:
+            raise ValueError('psd must be provided if not using autocorr')
+        # assert len(psd) == npsd
+        psd_final = np.copy(psd)
+
+    psd_final[0] = 0
+    scale = np.sqrt(psd_final * norm)
+
+    # gaussian Re/Im randoms, packed into a complex valued array
+
+    key1 = int(realization) * int(4294967296) + int(telescope) * int(65536)
+    key2 = int(sindx) * int(4294967296) + int(detindx)
+    counter1 = 0
+    counter2 = 0
+
+    rngdata = rng.random(
+        fftlen,
+        sampler='gaussian',
+        key=(key1, key2),
+        counter=(counter1, counter2),
+    ).array()
+
+    fdata = np.zeros(npsd, dtype=np.complex128)
+
+    # Set the DC and Nyquist frequency imaginary part to zero
+    fdata[0] = rngdata[0] + 0.0j
+    fdata[-1] = rngdata[npsd - 1] + 0.0j
+
+    # Repack the other values.
+    fdata[1:-1] = rngdata[1 : npsd - 1] + 1j * rngdata[-1 : npsd - 1 : -1]
+
+    # scale by PSD
+    fdata *= scale
+
+    # inverse FFT
+    tempdata = np.fft.irfft(fdata)
+
+    # subtract the DC level- for just the samples that we are returning
+    offset = (fftlen - samples) // 2
+
+    DC = np.mean(tempdata[offset : offset + samples])
+    tdata = tempdata[offset : offset + samples] - DC
+    return tdata
+
+
+def baseline(tod, valid, w0, rm=False, cp=False):
+    """
+    Computes a baseline (running average) for the given TOD.
+
+    Args:
+        tod (NDArray): The input timestream
+        valid (NDArray): An array where positive values indicate valid samples
+        w0 (int): The window of the running average has a width of 2*w0+1
+        rm (bool, optional): Subtract the baseline from the original TOD. Defaults to False.
+        cp (bool, optional): Return a copy of the timestream with the baseline removed, instead of removing it from the original one. Default to False.
+
+    Returns:
+        NDArray: The computed baseline
+        (NDArray, NDarray): The computed baseline and copied+modified TOD
+    """
+    samples = len(tod)
+    baseline = np.zeros(samples, dtype=float)
+    if cp:
+        work = np.copy(tod)
+        mappraiser.remove_baseline(samples, work, baseline, valid, w0, True)
+        return baseline, work
+
+    mappraiser.remove_baseline(samples, tod, baseline, valid, w0, rm)
+    return baseline
+
+
+# ____________________________________________________________
+# Gap-filling
+
+
+def sim_constrained_block(
+    init,
+    finalize,
+    w0,
+    tod,
+    tt,
+    invtt,
+    indices,
+    realization=0,
+    detindx=0,
+    sindx=0,
+    telescope=0,
+    fsamp=1.0,
+):
+    """
+    Computes a new TOD where gaps have been filled with a constrained realization of the noise.
+
+    Args:
+        init (_type_): _description_
+        finalize (_type_): _description_
+        w0 (_type_): _description_
+        tod (_type_): _description_
+        tt (_type_): _description_
+        invtt (_type_): _description_
+        indices (_type_): _description_
+        realization (int, optional): _description_. Defaults to 0.
+        detindx (int, optional): _description_. Defaults to 0.
+        sindx (int, optional): _description_. Defaults to 0.
+        telescope (int, optional): _description_. Defaults to 0.
+        fsamp (float, optional): _description_. Defaults to 1.0.
+
+    Returns:
+        NDArray: A copy of the input TOD with gaps filled.
+    """
+    samples = len(tod)
+    filled = np.copy(tod)
+    lambd = len(tt)
+    mappraiser.sim_constrained_block(
+        init,
+        finalize,
+        samples,
+        lambd,
+        w0,
+        tt,
+        invtt,
+        filled,
+        np.copy(indices),
+        realization,
+        detindx,
+        sindx,
+        telescope,
+        fsamp,
+    )
+    return filled
